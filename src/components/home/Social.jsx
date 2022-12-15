39b733774ed3a0a9539ea52c307058338eaed3cb
bleartdemaku <bd47165@ubt-uni.net>
2022-12-15 14:54:57 +0100

    rel="noreferrer" for older browser support

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function Social() {
   return (
     <div className="home__social">
-      <a href="https://www.instagram.com/bleartd/" className="home__social-icon" target="_blank">
+      <a href="https://www.instagram.com/bleartd/" className="home__social-icon" target="_blank" rel="noreferrer">
         <i class="uil uil-instagram"></i>
       </a>
 
-      <a href="https://www.linkedin.com/in/bleart-demaku-671a85251/" className="home__social-icon" target="_blank">
+      <a href="https://www.linkedin.com/in/bleart-demaku-671a85251/" className="home__social-icon" target="_blank" rel="noreferrer">
         <i class="uil uil-linkedin-alt"></i>
       </a>
 
-      <a href="https://github.com/bleartdemaku" className="home__social-icon" target="_blank">
+      <a href="https://github.com/bleartdemaku" className="home__social-icon" target="_blank" rel="noreferrer">
         <i class="uil uil-github-alt"></i>
       </a>
     </div>
