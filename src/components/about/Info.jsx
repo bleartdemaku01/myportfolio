5bb907f3179de3b7ae584b55350449dbd1cc512d
bleartdemaku <bd47165@ubt-uni.net>
2022-12-16 15:14:51 +0100

    finished About component

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./About.css";
+
+export default function info() {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">3 years working</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">10+ projects</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-support about__icon"></i>
+
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">online 24/7</span>
+      </div>
+    </div>
+  );
+}
