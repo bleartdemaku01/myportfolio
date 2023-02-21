aeeb09d486fd0fb4c33bd6443c9d6b283905da2c
bleartdemaku <bd47165@ubt-uni.net>
2023-02-21 16:59:50 +0100

    pjesa 1: Work

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Helmet } from "react-helmet";
 import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Work from "./components/work/Work";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Work />
         <Testimonials />
         <Contact />
       </main>
